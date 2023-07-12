e20b2b278768ed95b71edf0d48c69dc9eeaf63f5
Vladyslav Zyma <vladzima13@gmail.com>
2023-07-12 10:06:28 +0300

    added useReducer for app state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { useReducer } from "react";
+
 import Header from "./components/Header";
 import StartScreen from "./components/StartScreen";
 
+const initialState = {
+  questions: [],
+};
+
+function reducer(state, action) {}
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions } = state;
+
   return (
     <div className="app">
       <Header />
