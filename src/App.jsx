372fd1c557f4b6141e4823a41a0449ba3839c26e
Vladyslav Zyma <vladzima13@gmail.com>
2023-07-12 10:49:16 +0300

    removed AbortController from useQuestions, added request for questions in App component~

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
 import { useReducer } from "react";
 
+import useQuestions from "./hooks/useQuestions";
+
 import Header from "./components/Header";
 import StartScreen from "./components/StartScreen";
 
 const initialState = {
   questions: [],
+  //'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
 };
 
-function reducer(state, action) {}
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Wrong action");
+  }
+}
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { questions } = state;
+
+  useQuestions(dispatch);
 
   return (
     <div className="app">
--- src/hooks/useQuestions.js
+++ src/hooks/useQuestions.js
@@ -2,27 +2,22 @@ import { useEffect } from "react";
 
 function useQuestions(dispatch) {
   useEffect(() => {
-    const controller = new AbortController();
-
     async function fetchQuestions() {
       try {
-        const resp = await fetch("http://localhost:8000/questions", {
-          signal: controller.signal,
-        });
+        const resp = await fetch("http://localhost:8000/questions");
 
         if (!resp.ok) {
           throw new Error("Response not OK!");
         }
 
         const data = await resp.json();
+
         dispatch({ type: "dataReceived", payload: data });
       } catch (err) {
         dispatch({ type: "dataFailed" });
       }
     }
     fetchQuestions();
-
-    return () => controller.abort();
   }, [dispatch]);
 }
 
